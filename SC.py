fa11a6f4f383c547295522c500178b7f3b4fa4a2
ProgZiC <pavelavdeev412@gmail.com>
2023-12-17 12:16:05 +0300

    0.0.1

--- SC.py
+++ SC.py
@@ -4,36 +4,41 @@ from deepface import DeepFace
 
 def face_comparison(img_1,img_2):
     
-    result = DeepFace.verify(img1_path = img_1, img2_path = img_2, enforce_detection=False)
+    result = DeepFace.verify(img1_path = img_1, img2_path = img_2)
     
     if result.get('verified'):
-        print(result)
-        cv2.imwrite("q.png", img_1)
-def face_capture():
-    cascade_path = 'haarcascade_frontalface_default.xml'
-    clf = cv2.CascadeClassifier(cascade_path)
-    camera = cv2.VideoCapture('V.mp4')
+        return 1
+    else:
+        return 0
+        #cv2.imwrite("q.jpg", img_1) // запись изображения
+
+def face_capture_with_s():
+    cascade_path = 'haarcascade_frontalface_default.xml' # датасет
+    clf = cv2.CascadeClassifier(cascade_path) 
+    camera = cv2.VideoCapture('V.mp4') # источник потока
     
+    i=0
     while True:
         logic, frame = camera.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) // перевод картинки в серое 
         
         faces = clf.detectMultiScale(
-            gray,
+            frame,
             scaleFactor=1.1,
-            minNeighbors=10,
-            minSize=(30, 30),
+            minNeighbors=20,
+            minSize=(25, 25),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
         
         for (x, y, width, height) in faces:
             cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 255, 0), 2)
             crop_img = frame[y:y+height, x:x+width]
-            #cv2.imshow("cropped", crop_img)
-            cv2.imshow('Faces', frame)
-            #cv2.imwrite(str(i)+".png", crop_img)
+            cv2.imshow("cropped", crop_img)
+            #cv2.imshow('Faces', frame)
+
+            cv2.imwrite(str(i)+".jpg", crop_img)
             #image = cv2.imread("30.png")
-            
+            i+=1
             
         if cv2.waitKey(1) == ord('q'):
             break
@@ -43,12 +48,11 @@ def face_capture():
 
 
 def main():
-    #face_comparison('w2.png',"w1.png")
-    #face_capture()
-    objs = DeepFace.analyze(img_path = "30.png", 
- actions = ['age', 'gender', 'race', 'emotion']
-)
-    print(objs)
+    #print(face_comparison("2.jpg","1.jpg"))
+    face_capture_with_s()
+    #objs = DeepFace.analyze(img_path = "30.png", 
+    #actions = ['age', 'gender', 'race', 'emotion'])
+    #print(objs)
     
 if __name__ == '__main__':
     main()
