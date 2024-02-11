4f067f3092fa41a64a5861cdadc217cc1032029b
ProgZiC <pavelavdeev412@gmail.com>
2024-02-11 14:55:38 +0300

    don`t work

--- SC.py
+++ SC.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python
 import cv2
-#from deepface import DeepFace
+import dlib
 import argparse 
-
+import os
+import face_recognition
+import time
 parser = argparse.ArgumentParser(description='options')
 parser.add_argument(
     '-k',
@@ -20,24 +23,45 @@ namespace = parser.parse_args()
 
 def face_comparison(img_1,img_2):
     
-    result = DeepFace.verify(img1_path = img_1, img2_path = img_2)
-    
-    if result.get('verified'):
-        return 1
-    else:
-        return 0
-        #cv2.imwrite("q.jpg", img_1) // запись изображения
+    known_image = face_recognition.load_image_file("img_1.jpg")
+    unknown_image = face_recognition.load_image_file("img_2.jpg")
+
+    biden_encoding = face_recognition.face_encodings(known_image)[0]
+    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+    results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+    return results
+def input_database_slow():
+    db_dict={}
+    path=os.getcwd()
+    os.chdir(path+'\\Saved_frames')
+    names_picture=os.listdir()
+    i=0
+    k=1
+    for name in names_picture:
+        db_dict[k]=[name]
+        k+=1
+        for name_2 in names_picture:
+            if face_comparison(name,name_2)==1:
+                names_picture.remove(name_2)
+            else:
+                pass
+              
+    print(db_dict)
+             
 
 def face_capture_with_s(k,url):
     cascade_path = 'haarcascade_frontalface_default.xml' # датасет
+    
     clf = cv2.CascadeClassifier(cascade_path) 
+   
     camera = cv2.VideoCapture(0) # источник потока
     fps = 20.0
     camera.set(cv2.CAP_PROP_FPS, fps)
     i=0
 
-    import os
-    import time
+    
+    
     path=os.getcwd()
 
     if not os.path.isdir("Saved_frames"):
@@ -98,10 +122,10 @@ def face_capture_with_s(k,url):
 
 
 def main():
-
+    input_database_slow()
     #print(face_comparison("2.jpg","1.jpg"))
 
-    face_capture_with_s(namespace.k,namespace.indir)
+    #face_capture_with_s(namespace.k,namespace.indir)
 
     #objs = DeepFace.analyze(img_path = "30.png", 
     #actions = ['age', 'gender', 'race', 'emotion'])
