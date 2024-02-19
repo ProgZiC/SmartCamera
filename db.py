98453889b619ca5c9bad5d49f3588f34791ad5e0
ProZiC <pavelavdeev412@gmail.com>
2024-02-19 21:42:52 +0300

    table

--- db.py
+++ db.py
@@ -1,14 +1,37 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname="postgres", user="postgres", password="lamy", host="127.0.0.1")
+
+conn = psycopg2.connect(
+    user="postgres",
+    password="lamy",
+    host="127.0.0.1",
+    port="5432",)
 cursor = conn.cursor()
 conn.autocommit = True
     
-def enter():
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+classes TEXT NOT NULL,
+birthday TEXT,
+gender TEXT,
+image TEXT NOT NULL
+)
+''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS log (
+id SERIAL PRIMARY KEY,
+key TEXT NOT NULL,
+data TEXT NOT NULL,
+clock TEXT NOT NULL,
+image TEXT NOT NULL
+)''')
+#conn.commit()
+ 
 
-    sql1 = 'CREATE DATABASE Humans'
-    cursor.execute(sql1)
- 
- 
+def in_user(name,group,birthday,gender,image):
+    cursor.execute('INSERT INTO Users(name,classes,birthday,gender,image) VALUES (%s,%s,%s,%s,%s)',(name,group,birthday,gender,image))
+#in_user('ivan','FEBO','21.03.2005','M','132.jpg')
 cursor.close()  # закрываем курсор
 conn.close()    # закрываем подключение 
